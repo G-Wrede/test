319b4bd82a53a3ad1114cddb3af617c78bc5cd53
gwrede <35573680+gwrede@users.noreply.github.com>
2019-04-01 13:09:53 +0200

    Update V12 last 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include <Arduino.h>
 /*
   Würfel mit 5 Augen
-  mit Zufall und Statistik
-  Lösung der Übungsaufgabe
-
-  Version 1.0
-  Der Hobbyelektroniker
-  Der Code ist Public Domain und kann ohne Einschränkungen frei verwendet werden
 */
 
 // Anordnung der LED's
@@ -16,16 +10,21 @@
  * 4     3
  */
 
-// void blink(int, int, int, bool);
 const int anzahl_augen = 5;
-const int ledPin[anzahl_augen] = {2, 3, 4, 5, 6};
-const int tasterPin = 7;
-const int ledInterval = 1000;
-const int blinkTime = 100;
-unsigned long curtime = 0;
-int resettimer = 0;
-
-void blink(int led, int blTime, int durTime, bool ledReset ) {
+const int ledPin[anzahl_augen] = {2, 3, 4, 5, 6}; // Arduino Pins
+const int tasterPin = 7;        // Eingang fuer den Taster
+const int ledInterval = 15000;  // wechselintervall zum naechsten Led in ms
+const int blinkTime = 100;      // Blinkintervall der Leds
+unsigned long tic = 0;         // speichert die Zeit seit dem Start in ms
+bool resettimer = 0;            // Wenn gesetzt dann wird der Ausgang zurueck gesetzt
+/*
+Parameter:
+  led     = Led Nummer
+  blTime  = blinkdauer in ms
+  durTime = dauer des blinkens danach bleibt das Led an
+  ledReset= wenn 1 dann wird der Ausgang zurueck gesetzt
+ */
+void blink(int led, int blTime, int durTime, bool ledReset) {
   static int ledTime[anzahl_augen] = {0};
   static unsigned long startzeit[anzahl_augen] = {0};
   static unsigned long startDurzeit[anzahl_augen] = {0};
@@ -36,33 +35,34 @@ void blink(int led, int blTime, int durTime, bool ledReset ) {
     startzeit[led] = 0;
     startDurzeit[led] = 0;
   } else if (startDurzeit[led] == 0) {
-    startDurzeit[led] = curtime;
-  } else if (curtime - startDurzeit[led] > ledTime[led] && ledTime[led] > 0) {
+    startDurzeit[led] = tic;
+  } else if (tic - startDurzeit[led] > ledTime[led] && ledTime[led] > 0) {
     digitalWrite(ledPin[led], HIGH);
-  } else if ((curtime - startzeit[led]) > blTime) {
+  } else if ((tic - startzeit[led]) > blTime) {
     digitalWrite(ledPin[led], !digitalRead(ledPin[led]));
-    startzeit[led] = curtime;
+    startzeit[led] = tic;
   }
 }
 
 void taster() {
   if (!digitalRead(tasterPin)) {
-    resettimer = 1;
-    delay(100);
+    resettimer = 1;     // setzen des Reset auf HIGH
+    delay(100);         // entcoppeln
   }
 }
 
 void start() {
-  static unsigned long starttimer = curtime;
+  static unsigned long starttimer = tic;
   int i = 0;
   blink(0, 500, 0, 0);
-  for(i = 0; i < anzahl_augen; i++) {
-    if (curtime - starttimer > ledInterval * i)
-    blink(i + 1, blinkTime, ledInterval, 0);
+  for (i = 0; i < anzahl_augen; i++) {
+    if (tic - starttimer > ledInterval * i)
+      blink(i + 1, blinkTime, ledInterval, 0);
   }
-  if (curtime - starttimer > ledInterval * (anzahl_augen -  1) || resettimer) {
+  if (tic - starttimer >= ledInterval * (anzahl_augen - 1) || resettimer) {
+    Serial.print(starttimer > ledInterval * (anzahl_augen - 1));
     resettimer = 0;
-    starttimer = curtime;
+    starttimer = tic;
     for (int j = 0; j < anzahl_augen; j++) {
       blink(j, 0, 0, 1);
     }
@@ -78,7 +78,7 @@ void setup() {
 }
 
 void loop() {
-  curtime = millis();
+  tic = millis();
   taster();
   start();
 }
